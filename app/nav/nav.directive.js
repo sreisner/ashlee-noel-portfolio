1cce969ed8e10c6c244604310241ff31287d48ca
Shawn Reisner <shawn.reisner@gmail.com>
2016-07-19 17:46:12 -0500

    Change background in nav directive based on screen width

--- app/nav/nav.directive.js
+++ app/nav/nav.directive.js
@@ -5,7 +5,22 @@
   app.directive('appNav', function() {
     return {
       restrict: 'E',
-      templateUrl: '/static/nav/nav.tmpl.min.html'
+      templateUrl: '/static/nav/nav.tmpl.min.html',
+      link: function(_, element) {
+        var img = element.find('#logo-img')[0];
+        setLogoImage(img);
+        window.onresize = function(event) {
+          setLogoImage(img);
+        }
+      }
     };
   });
+
+  function setLogoImage(imgElement) {
+    if(window.innerWidth < 800) {
+      imgElement.src = '/images/mobile logo black.png';
+    } else {
+      imgElement.src = '/images/scrolled-logo-black.png';
+    }
+  }
 })();
